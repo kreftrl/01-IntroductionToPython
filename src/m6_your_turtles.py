6bb5efa6fd7b81eb10b82d6b2b7076ddfe2c1818
kreftrl <kreftrl@rose-hulman.edu>
2018-11-26 13:21:14 -0500

    done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Robert Kreft.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,27 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window=rg.TurtleWindow()
+bob=rg.SimpleTurtle('turtle')
+bobjr=rg.SimpleTurtle('turtle')
+bobmcbob=rg.SimpleTurtle('turtle')
+bobsr=rg.SimpleTurtle('turtle')
+bob.pen=rg.Pen('red',2)
+bobjr.pen=rg.Pen('blue',2)
+bobmcbob.pen=rg.Pen('maroon',2)
+bobsr.pen=rg.Pen('purple',2)
+window.tracer(5)
+for k in range (100):
+    bobjr.right(180)
+    bobmcbob.right(90)
+    bobsr.right(270)
+    bob.forward(k)
+    bobjr.forward(k)
+    bob.right(k)
+    bobjr.right(k-180)
+    bobmcbob.forward(k)
+    bobmcbob.right(k-90)
+    bobsr.forward(k)
+    bobsr.right(k-270)
+window.close_on_mouse_click()
